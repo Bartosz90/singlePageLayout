0be756599b569c6eb2257a545e37f081732e15b0
Bartek Krupa <krupa1.bk@gmail.com>
2019-09-15 13:13:10 +0200

    add nav functionalities

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,31 @@
 //generate random background color for each section
-const colors = [
-  "0",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f"
-];
-const sections = document.querySelectorAll(".section");
-sections.forEach(section => {
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color +=
-      colors[Math.floor(Math.random() * (colors.length - 1 - 0 + 1)) + 0];
-  }
-  section.style.backgroundColor = `${color}`;
-});
+// const colors = [
+//   "0",
+//   "1",
+//   "2",
+//   "3",
+//   "4",
+//   "5",
+//   "6",
+//   "7",
+//   "8",
+//   "9",
+//   "a",
+//   "b",
+//   "c",
+//   "d",
+//   "e",
+//   "f"
+// ];
+// const sections = document.querySelectorAll(".section");
+// sections.forEach(section => {
+//   let color = "#";
+//   for (let i = 0; i < 6; i++) {
+//     color +=
+//       colors[Math.floor(Math.random() * (colors.length - 1 - 0 + 1)) + 0];
+//   }
+//   section.style.backgroundColor = `${color}`;
+// });
 
 // navigation
 const main = document.querySelector(".main");
@@ -36,7 +36,12 @@ navBtns.forEach(btn => {
   btn.addEventListener("click", moveToSection);
 });
 function moveToSection(e) {
+  navBtns.forEach(btn => {
+    btn.classList.remove("active");
+  });
   main.style.transform = `translate(${e.target.dataset.x}%, ${e.target.dataset.y}%)`;
+  e.target.classList.add("active");
+  nav.classList.remove("active");
 }
 hamburger.addEventListener("click", () => {
   nav.classList.toggle("active");
